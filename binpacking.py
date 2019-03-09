e09a4f5a102d268e7d0a0037ea48189e35e8e109
mccabmic <mccabmic@oregonstate.edu>
2019-03-08 18:13:37 -0600

    upload for remote working

---
+++ binpacking.py
@@ -0,0 +1,46 @@
+class Bin(object):
+    def __init__(self):
+        self.items = []
+        self.sum = 0
+
+    def append(self, item):
+        self.items.append(item)
+        self.sum += item
+
+    def __str__(self):
+        """ Printable representation """
+        return 'Bin(sum=%d, items=%s)' % (self.sum, str(self.items))
+    
+    def __repr__(self):
+        """ Printable representation """
+        return 'Bin(sum=%d, items=%s)' % (self.sum, str(self.items))
+
+
+def first_fit(items, bin_size):
+    bins = []
+    for item in items:
+        for bin in bins:
+            if bin.sum + item <= bin_size:
+                bin.append(item)
+                break
+        else:
+            bin = Bin()
+            bin.append(item)
+            bins.append(bin)
+
+    return bins
+
+def first_fit_d(items, bin_size):
+    values = sorted(items, reverse=True)
+    return (first_fit(values, bin_size))
+
+# weights = [3, 8, 2, 7]
+# weights = [5, 10, 2, 5, 4, 4]
+weights = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
+bin_size = 10
+
+n_bins = first_fit(weights, bin_size)
+n_bins_d = first_fit_d(weights, bin_size)
+
+print(len(n_bins))
+print(len(n_bins_d))
